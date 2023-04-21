a6df5256a58a067f56433d74d37c7d24d0a98586
KesterDaniel <kesterdan17@gmail.com>
2023-04-21 14:01:57 +0100

    God abeg

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 Base = declarative_base()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 bootstrap = Bootstrap5(app)
 login_manager = LoginManager()
@@ -26,7 +26,7 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
