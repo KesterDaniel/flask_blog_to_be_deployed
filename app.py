5281d3c1cbe239ef9f587368059fac05148e4553
KesterDaniel <kesterdan17@gmail.com>
2023-04-21 13:40:54 +0100

    third commit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'sqlite:///posts.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
